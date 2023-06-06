129aa693f235c490ea058bd9d4752c8b0c244865
abhishek <avisec07@gmail.com>
2023-06-06 20:24:06 +0530

    db config

--- Server/DatabaseConfig.go
+++ Server/DatabaseConfig.go
@@ -2,14 +2,28 @@ package Server
 
 import (
 	"AIDS_Trigger/Message"
+	"encoding/json"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
+
+type DatabaseInfo struct {
+	Username string
+	Password string
+	Host     string
+	Database string
+}
 
 func InitializeDB() *gorm.DB {
 	//dsn := "admin:password@tcp(aids-db.cbo2bmr3r0wv.ap-south-1.rds.amazonaws.com:3306)/aids_db?parseTime=true"
-	dsn := "root:admin@tcp(127.0.0.1:3306)/aids_db?charset=utf8mb4&parseTime=True&loc=Local"
+	var databaseInfo DatabaseInfo = DatabaseInfo{}
+	dbConfig, err := os.ReadFile("./db.json")
+	err = json.Unmarshal(dbConfig, &databaseInfo)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		databaseInfo.Username, databaseInfo.Password, databaseInfo.Host, databaseInfo.Database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
